b8c490311b87545e30f815057d0a62017f397212
Victor Penso <v.penso@gsi.de>
2014-06-26 10:14:49 +0200

    Use 64Bit traffic counter, and average values over measurment period

--- lib/python_modules/infiniband.py
+++ lib/python_modules/infiniband.py
@@ -17,12 +17,16 @@
 #
 
 import time
+import logging
+import time
 import subprocess
 import re
 
-perfquery = {}
+# Data structure used to hold all monitoring metrics 
+# collected by this sensor
+metrics = {}
 
-# Identify the number of ports on the Infiniband adapter
+# Identify the ports and LIDs of the Infiniband adapters
 def ibstat_ports():
     ports = {}
     ibstat = subprocess.Popen("ibstat", stdout=subprocess.PIPE).stdout.readlines()
@@ -35,89 +39,131 @@ def ibstat_ports():
           ports[number] = lid
     return ports
 
-def update_metrics(time_max = 20):
-    # Buffer for performance metrics
-    global perfquery
-    # Update metrics only if defined live time is exceeded 
-    if (time.time() - perfquery["time"]) > time_max:
-        # Iterate over all Infiniband ports
-        for port,lid in ibstat_ports().items():
-            # Reset metrics structure
-            perfquery[port] = {}
-            # Use perfquery to read Infiniband port counters
-            command = ["sudo","/usr/sbin/perfquery", "-r",lid, port, "0xf000"] 
-            # Reset the perfquery counters (with exception of errors)
-            trash = subprocess.Popen(command, stdout=subprocess.PIPE).stdout.readlines()
-            # accumulate counters for one second
-            begin = time.time()
-            time.sleep(1)
-            end = time.time()
-            # Sleep in Python is not real-time, means that depending on system interrupts
-            # the relapsed time-frame is bigger then a second
-            elapsed = end - begin
-            # read the metrics for the past second
-            perf_data = subprocess.Popen(command, stdout=subprocess.PIPE).stdout.readlines()
-            for line in perf_data:
-               line = line.split(':')
-               key = line[0]
-               value = line[1]
-               value = value.replace('.','').strip()
-               if key == "RcvPkts":
-                  perfquery[port]["rcvpkts"] = int(float(value)/elapsed)
-               elif key == "XmtPkts":
-                  perfquery[port]["xmtpkts"] = int(float(value)/elapsed)
-               # Data port counters indicate octets divided by 4 rather than just octets.
-               #
-               # It's consistent with what the IB spec says (IBA 1.2 vol 1 p.948) as to
-               # how these quantities are counted. They are defined to be octets divided
-               # by 4 so the choice is to display them the same as the actual quantity
-               # (which is why they are named Data rather than Octets) or to multiply by
-               # 4 for Octets. The former choice was made.
-               #
-               # For simplification the values are multiplied by for to represent 
-               # octets/bytes, for better visualization on in graphs "bytes/second"
-               elif key == "RcvData":
-                  perfquery[port]["rcvdata"] = int((float(value)*4)/elapsed)
-               elif key == "XmtData":
-                  perfquery[port]["xmtdata"] = int((float(value)*4)/elapsed)
-               elif key == "SymbolErrors":
-                  perfquery[port]["symbolerrors"] = value
-               elif key == "LinkRecovers":
-                  perfquery[port]["linkrecovers"] = value
-               elif key == "LinkDowned":
-                  perfquery[port]["linkdowned"] = value
-               elif key == "RcvErrors":
-                  perfquery[port]["rcverrors"] = value
-               elif key == "RcvRemotePhysErrors":
-                  perfquery[port]["rcvremotephyserrors"] = value
-               elif key == "RcvSwRelayErrors":
-                  perfquery[port]["rcvswrelayerrors"] = value
-               elif key == "XmtDiscards":
-                  perfquery[port]["xmtdiscards"] = value
-               elif key == "XmtConstraintErrors":
-                  perfquery[port]["xmtconstrainterrors"] = value
-               elif key == "RcvConstraintErrors":
-                  perfquery[port]["rcvconstrainterrors"] = value
-               elif key == "LinkIntegrityErrors":
-                  perfquery[port]["linkintegrityerrors"] = value
-               elif key == "ExcBufOverrunErrors":
-                  perfquery[port]["excbufoverrunerrors"] = value
-               elif key == "VL15Dropped":
-                  perfquery[port]["vl15dropped"] = value
-            # Buffer a time stamp of the last time metrics have been recorded 
-            perfquery["time"] = time.time()
+# Match input line to be a counter, e.g.:
+#
+#    PortXmitData:....................405429
+#
+# Return a key-value pair, eventually empty if the line didn't match 
+def parse_counter_line(line):
+    if re.match("^[a-zA-z0-9]*\:\.\.\.*[0-9]*$",line):
+        line = line.split(':')
+        key = line[0]
+        # Ignore 32Bit traffic counters
+        if not key in ["XmtData","RcvData","XmtPkts","RcvPkts"]:
+            value = line[1].replace('.','').strip()
+            return (key.lower(), int(value))
+    return ("",0) 
+
+# Parse the complete input from perfquery for lines matching counters,
+# and return all counters and their values as dictionary
+def parse_counters(counters):
+    counts = {}
+    for line in counters:
+        key, value = parse_counter_line(line)
+        # Omit empty return values...
+        if key:
+          counts[key] = value
+    return counts
+
+# Call perfquery for error counters
+def error_counter(lid, port = 1):
+    command = ["sudo", "/usr/sbin/perfquery", "-r", lid, port, "0xf000"] 
+    logging.debug("Execute command: %s", " ".join(command))
+    counters = subprocess.Popen(command, stdout=subprocess.PIPE).stdout.readlines()
+    return parse_counters(counters) 
+  
+# Call perfquery for extended traffic counters, and reset the counters
+def traffic_counter(lid, port = 1):
+    command = ["sudo", "/usr/sbin/perfquery", "-e", lid, port] 
+    logging.debug("Execute command: %s", " ".join(command))
+    counters = subprocess.Popen(command, stdout=subprocess.PIPE).stdout.readlines()
+    return parse_counters(counters) 
+
+def update_metrics():
+    
+    global metrics
+
+    time_since_last_update = time.time() - metrics["last_update"]
+
+    # Update metric after their maximum life time is exceeded 
+    if time_since_last_update > metrics["maximum_life_time"] - 5:
+    # This is useful to prevent updated for every call to the metric_handler()
+    # function, too.
+
+       logging.debug("Update metrics after %ss", time_since_last_update)
+
+       # Iterate over all Infiniband ports
+       for port,lid in ibstat_ports().items():
+           
+           # Call perfquery to collect counters
+           traffic = traffic_counter(lid, port)
+           # Order matters, since the function to read the error counters resets
+           # the traffic counters.
+           errors = error_counter(lid, port)
+           # Unfortunately reset of the counters with a mask doesn't work with
+           # the extended option of perfquery in the traffic_counter() function!!
+           
+           # Store counters for this port
+           metrics[port] = dict(errors.items() + traffic.items())
+           
+           # Data port counters indicate octets divided by 4 rather than just octets.
+           #
+           # It's consistent with what the IB spec says (IBA 1.2 vol 1 p.948) as to
+           # how these quantities are counted. They are defined to be octets divided
+           # by 4 so the choice is to display them the same as the actual quantity
+           # (which is why they are named Data rather than Octets) or to multiply by
+           # 4 for Octets. The former choice was made.
+           #
+           # For simplification the values are multiplied by for to represent octets/bytes
+           data_counters = [ "portxmitdata","portrcvdata" ]
+           for metric in data_counters:
+               # Multiply by four to calculate bytes
+               octets = metrics[port][metric]*4
+               # Remove the original counter from the metrics list
+               metrics[port].pop(metric, None)
+               # New key to represent bytes per second 
+               key = metric.replace("data","bytes") + "sec"
+               # Divide package counter by seconds since last measurement
+               metrics[port][key] = octets/time_since_last_update
+
+           # Calculate packages per second for all package counters
+           package_counters = [
+               "portxmitpkts", "portrcvpkts",
+               "portunicastxmitpkts", "portunicastrcvpkts",
+               "portmulticastxmitpkts", "portmulticastrcvpkts"
+           ]
+           for metric in package_counters:
+               # Append seconds to the counter name
+               key = metric + "sec"
+               # Divide package counter by seconds since last measurement
+               metrics[port][key] = metrics[port][metric]/time_since_last_update
+               # Remove the original counter from the metrics list
+               metrics[port].pop(metric,None)
+           
+
+    # Buffer a time stamp of the last time metrics have been recorded 
+    metrics["last_update"] = time.time()
+    # Set values for individual metrics
+    metrics["example"] = 123
 
 # It takes one parameter, 'name', which is the value defined 
 # in the 'name' element in your metric descriptor. 
-def metric_handler(name):
-    global perfquery
+def metric_handler(metric):
+
+    global metrics
+
+    # Make sure to update metrics if required
     update_metrics()
-    name = name.split('_')
-    metric = name[1]
-    port = name[2]
-    port = port.replace('port','')
-    value = float(perfquery[port][metric])
-    return value
+
+    # Extract the metric name and port from the metric 
+    # descriptor name
+    metric = metric.split('_')
+    key = metric[1]
+    port = metric[2]
+    port = port.replace("port","")
+
+    # Return the value for the queried metric
+    return metrics[port][key]
             
 
 # This function must exist!  
@@ -131,82 +177,61 @@ def metric_handler(name):
 # configuration directives that were designated for this 
 # module in the gmond.conf file.
 def metric_init(params):
+    
+    global logger
+    global metrics
 
-    global perfquery
+    maximum_life_time = int(params["maximum_life_time"])
 
     # Initialize the first time stamp in the past to make sure 
     # that the perfquery data gets initialized on first execution
-    perfquery["time"] = time.time() - int(params["interval"])
-    update_metrics()
+    metrics["last_update"] = time.time() - maximum_life_time
+    # Maximum live time of metrics recorded by this module 
+    metrics["maximum_life_time"] = maximum_life_time
 
     # The group in the web front-end with which this metric will 
     # be associated 
-    group = "infiniband"
-    # Maximum live time of metrics recorded by this module 
-    #time_max = int(params["interval"])
-    time_max = 60
-
+    metric_group = "infiniband"
+    
     # Metric description dictionary, will be returned to the 
-    # module caller, basically gmond 
+    # module caller Gmond. 
     descriptors = list()
+    
+    update_metrics()
 
     # Iterate over all Infiniband ports
     for port,lid in ibstat_ports().items():
-        
-        # Prefix for the metric name, common to all metrics
-        # supported by this module
-        port = "_port" + port
 
-        # Gather the minimal set of performance metrics related
-        # to the network traffic per seconds: packets, bytes
-        traffic_metrics = {
-            "RcvPkts": "packets/sec", 
-            "XmtPkts": "packets/sec", 
-            "RcvData": "bytes/sec", 
-            "XmtData": "bytes/sec"
-        }
-        # 
-        for name,fmt in traffic_metrics.items():
-            metric               = {}
-            metric["name"]       = group + "_" + name.lower() + port
-            metric["call_back"]  = metric_handler
-            metric["time_max"]   = time_max
-            metric["value_type"] = "float"
-            metric["format"]     = '%.0f'
-            metric["slope"]      = "both"
-            metric["units"]      = fmt
-            metric["groups"]     = group
+        for key in metrics[port]:
+
+            # Determine the unit format for all metrics 
+            if key in ["portrcvbytessec","portxmitbytessec"]:
+                unit = "bytes/sec"
+            elif key in [
+                "portxmitpktssec","portrcvpktssec"
+                "portunicastxmitpktssec", "portunicastrcvpktssec",
+                " portmulticastxmitpktssec", "portmulticastrcvpktssec" ]:
+                unit = "packets/sec"
+            else:
+                unit = "counter"
+
+            # Metric descriptor name
+            name = metric_group + "_" + key + "_port" + port
+            # Define a metric
+            metric = {
+                "name":       name,
+                "call_back":  metric_handler,
+                "time_max":   maximum_life_time + 10,
+                "value_type": "float",
+                "format":     "%.0f",
+                "slope":      "both",
+                "units":      unit,
+                "groups":     metric_group
+            };
+            # Add this metric to the descriptor list
+            logging.debug("Register metric %s %s", metric["name"], unit)
             # Add this metric to the descriptor list
             descriptors.append(metric)
-
-        # If module parameters enable collection of error metrics
-        infiniband_error_metrics = [ 
-            "SymbolErrors", 
-            "LinkRecovers", 
-            "LinkDowned", 
-            "RcvErrors", 
-            "RcvRemotePhysErrors", 
-            "RcvSwRelayErrors", 
-            "XmtDiscards", 
-            "XmtConstraintErrors", 
-            "RcvConstraintErrors", 
-            "LinkIntegrityErrors", 
-            "ExcBufOverrunErrors", 
-            "VL15Dropped" 
-        ]
-        if params["error_metrics"] == "yes":
-            for name in infiniband_error_metrics:
-                metric               = {}
-                metric["name"]       = group + "_" + name.lower() + port
-                metric["call_back"]  = metric_handler
-                metric["time_max"]   = time_max
-                metric["value_type"] = "float"
-                metric["format"]     = '%.0f'
-                metric["slope"]      = "both"
-                metric["units"]      = "number of"
-                metric["groups"]     = group
-                # Add this metric to the descriptor list
-                descriptors.append(metric)
 
     # Register metrics provided by this module
     return descriptors
@@ -221,7 +246,11 @@ def metric_cleanup():
 
 if __name__ == '__main__':
 
-    params = { "interval": "20", "error_metrics": "yes" }
+    logging.root.setLevel(logging.DEBUG) 
+
+    params = { 
+        "maximum_life_time": "20"
+    }
 
     descriptors = metric_init(params)
 
@@ -229,4 +258,5 @@ if __name__ == '__main__':
         for d in descriptors:
             v = d['call_back'](d['name'])
             print 'value for %s is %u' % (d['name'],  v)
-        time.sleep(int(params["interval"]))
+
+        time.sleep( int(params["maximum_life_time"]) - 1 )
